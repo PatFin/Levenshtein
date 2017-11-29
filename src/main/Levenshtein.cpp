cdc43c3847dcd80a5d9268b71e71ac19d18c9578
Patrick Finnerty <finnertypatri@aol.fr>
2017-11-29 21:08:02 +0900

    Moved Variables in the Levenshtein namespace Removed the using namespace std; and performed the appropriate changes in the file Corrected an error : was using variable for insertions/deletion cost rather than the mismatch cost

--- src/main/Levenshtein.cpp
+++ src/main/Levenshtein.cpp
@@ -2,20 +2,24 @@
 #include <string>
 #include <stack>
 
-using namespace std;
-
-
+// Namespace for types used in the Levenshtein distance computation
 namespace Levenshtein
 {
 	typedef char Backtrack;
+	// Backtrack characters
+	static const Backtrack ROOT = 'R';
+	static const Backtrack TOP = 'T';
+	static const Backtrack LEFT = 'L';
+	static const Backtrack DIAGONAL = 'D';
+
+	// Costs for the different modifications possible
+	static const int MISMATCH_COST = -1;
+	static const int INDEL_COST = -1;
+	static const int MATCH_COST = 1;
+
 	class ScoreBack
 	{
 		public :
-		// Backtrack characters
-		static const Backtrack ROOT = 'R';
-		static const Backtrack TOP = 'T';
-		static const Backtrack LEFT = 'L';
-		static const Backtrack DIAGONAL = 'D';
 
 		int score;
 		Backtrack back;
@@ -28,15 +32,11 @@ namespace Levenshtein
 	};
 }
 //---------------------- PARAMETERS OF THE ALGORITHM ---------------------------
-// Costs for the different modifications possible
-const int MISMATCH_COST = -1;
-const int INDEL_COST = -1;
-const int MATCH_COST = 1;
 
 //----------------------------------VARIABLES-----------------------------------
 // Variables shared among procedures
-string a;			// first word
-string b;			// second word
+std::string a;			// first word
+std::string b;			// second word
 
 int WIDTH;			// width of the score matrix = first word length + 1
 int HEIGHT;			// height of the score matrix = 2nd word length + 1
@@ -53,8 +53,8 @@ void displayAlignment();
 //---------------------------------PROCEDURES-----------------------------------
 void init ( )
 {
-	getline(cin,a);
-	getline(cin,b);
+	getline(std::cin,a);
+	getline(std::cin,b);
 
 	WIDTH = a.length() + 1;
 	HEIGHT = b.length() + 1;
@@ -82,9 +82,9 @@ void displayScore ()
 	{
 		for (int j = 0; j < WIDTH; j++)
 		{
-			cout << scoreBack[j][i].score << " ";
+			std::cout << scoreBack[j][i].score << " ";
 		}
-		cout << endl ;
+		std::cout << std::endl ;
 	}
 }
 
@@ -94,9 +94,9 @@ void displayBackTrack ()
 	{
 		for (int j = 0; j < WIDTH; j++)
 		{
-			cout << scoreBack[j][i].back << " ";
+			std::cout << scoreBack[j][i].back << " ";
 		}
-		cout << endl ;
+		std::cout << std::endl ;
 	}
 }
 
@@ -108,14 +108,16 @@ void computeScoreAndBacktrack ()
 	// Computing the first column
 	for (i = 0, j = 1; j < HEIGHT; j++)
 	{
-		scoreBack [i][j].score = scoreBack [i][j-1].score + INDEL_COST;
-		scoreBack [i][j].back = Levenshtein::ScoreBack::TOP;
+		scoreBack [i][j].score = scoreBack [i][j-1].score +
+			Levenshtein::INDEL_COST;
+		scoreBack [i][j].back = Levenshtein::TOP;
 	}
 	// Computing the first line
 	for (i = 1, j = 0; i < WIDTH; i++)
 	{
-		scoreBack [i][j].score = scoreBack [i-1][j].score + INDEL_COST;
-		scoreBack [i][j].back = Levenshtein::ScoreBack::LEFT;
+		scoreBack [i][j].score = scoreBack [i-1][j].score +
+			Levenshtein::INDEL_COST;
+		scoreBack [i][j].back = Levenshtein::LEFT;
 	}
 
 	// Computing the rest of the score
@@ -123,37 +125,39 @@ void computeScoreAndBacktrack ()
 	{
 		for (j = 1; j < HEIGHT; j++) {
 			// Compute the three candidates
-			int top = scoreBack [i][j-1].score + INDEL_COST;
+			int top = scoreBack [i][j-1].score + Levenshtein::INDEL_COST;
 
-			int left = scoreBack [i-1][j].score + INDEL_COST;
+			int left = scoreBack [i-1][j].score + Levenshtein::INDEL_COST;
 
 			int topLeft;
 			if ( a[i-1] == b[j-1] )
 			{
-				topLeft = scoreBack [i-1][j-1].score + MATCH_COST;
+				topLeft = scoreBack [i-1][j-1].score + Levenshtein::MATCH_COST;
 			} else {
-				topLeft = scoreBack [i-1][j-1].score + INDEL_COST;
+				topLeft = scoreBack [i-1][j-1].score +
+						Levenshtein::MISMATCH_COST;
 			}
 			#ifdef DEBUG
-			cout << "Top:" << top << " Diag:" << topLeft << " Left:" << left;
+			std::cout << "Top:" << top << " Diag:" << topLeft << " Left:"
+					<< left;
 			#endif
 			// Pick the highest value of them three
-			char back = Levenshtein::ScoreBack::TOP;
+			char back = Levenshtein::TOP;
 			if (top < left)
 			{
 				top = left;
-				back = Levenshtein::ScoreBack::LEFT;
+				back = Levenshtein::LEFT;
 			}
 			if ( top < topLeft )
 			{
 				top = topLeft;
-				back = Levenshtein::ScoreBack::DIAGONAL;
+				back = Levenshtein::DIAGONAL;
 			}
 			scoreBack [i][j].score = top;
 			scoreBack [i][j].back = back;
 			#ifdef DEBUG
-			cout << " BEST:" << scoreBack [i][j].score << " BACK:" <<
-					scoreBack[i][j].back << endl;
+			std::cout << " BEST:" << scoreBack [i][j].score << " BACK:" <<
+					scoreBack[i][j].back << std::endl;
 			#endif
 		}
 	}
@@ -162,23 +166,23 @@ void computeScoreAndBacktrack ()
 void displayAlignment ( )
 {
 
-	stack <char> action;
+	std::stack <char> action;
 	int i = WIDTH-1;
 	int j = HEIGHT-1;
 	while ( i != 0 || j != 0)
 	{
 		action.push(scoreBack[i][j].back);
-		//cout << action.top() << endl;
+		//std::cout << action.top() << std::endl;
 		switch (action.top())
 		{
-			case Levenshtein::ScoreBack::DIAGONAL:
+			case Levenshtein::DIAGONAL:
 				i--;
 				j--;
 			break;
-			case Levenshtein::ScoreBack::TOP :
+			case Levenshtein::TOP :
 				j--;
 			break;
-			case Levenshtein::ScoreBack::LEFT :
+			case Levenshtein::LEFT :
 				i--;
 			break;
 		}
@@ -186,7 +190,7 @@ void displayAlignment ( )
 
 	int stringOutputSize = action.size();
 #ifdef DEBUG
-	cout << "Alignment length:" << action.size() << endl;
+	std::cout << "Alignment length:" << action.size() << std::endl;
 #endif
 	char x [stringOutputSize];
 	char y [stringOutputSize];
@@ -196,18 +200,18 @@ void displayAlignment ( )
 	{
 		switch (action.top())
 		{
-			case Levenshtein::ScoreBack::DIAGONAL:
+			case Levenshtein::DIAGONAL:
 				x[k] = a[i];
 				y[k] = b[j];
 				i++;
 				j++;
 			break;
-			case Levenshtein::ScoreBack::TOP :
+			case Levenshtein::TOP :
 				x[k] = '-';
 				y[k] = b[j];
 				j++;
 			break;
-			case Levenshtein::ScoreBack::LEFT :
+			case Levenshtein::LEFT :
 				x[k] = a[i];
 				y[k] = '-';
 				i++;
@@ -219,13 +223,13 @@ void displayAlignment ( )
 
 
 	for (int z = 0; z < stringOutputSize; z++) {
-		cout << x[z];
+		std::cout << x[z];
 	}
-	cout << endl;
+	std::cout << std::endl;
 	for (int z = 0; z < stringOutputSize; z++) {
-		cout << y[z];
+		std::cout << y[z];
 	}
-	cout << endl;
+	std::cout << std::endl;
 }
 
 //-----------------------------------MAIN---------------------------------------
@@ -239,7 +243,7 @@ int main (void)
 	displayBackTrack();
 #endif
 
-	cout << "Score:" << scoreBack[WIDTH-1][HEIGHT-1].score << endl;
+	std::cout << "Score:" << scoreBack[WIDTH-1][HEIGHT-1].score << std::endl;
 	displayAlignment();
 	end ();
 	return 0;
