a387d278ea850d8f72d02587eaf3ffb714c7c477
Patrick Finnerty <finnertypatri@aol.fr>
2017-11-30 17:21:16 +0900

    Added optional alignment display and help display

--- src/main/Levenshtein.cpp
+++ src/main/Levenshtein.cpp
@@ -1,19 +1,42 @@
 #include <iostream>
 #include <string>
-#include <stack>
+#include <unistd.h>
 
 #include "Computer.h"
 
 
 //----------------------------------VARIABLES-----------------------------------
+bool AlignOption = false;
+//bool DistanceOption = false;
 
-
-//----------------------------PROCEDURE DECLARATIONS----------------------------
-
-
-//-----------------------------------MAIN---------------------------------------
-int main (void)
+//--------------------------------PROCEDURES------------------------------------
+void displayHelp ( );
+void displayHelp ( )
 {
+	std::cout << "Usage : Levenshtein [-a]" << std::endl;
+}
+//-----------------------------------MAIN---------------------------------------
+int main (int argc, char ** args)
+{
+	int i = 1;
+	char option;
+	while ( (option = getopt(argc, args, "ah")) != -1 )
+	{
+		switch ( option )
+		{
+			case 'a' :
+				AlignOption = true;
+				break;
+			case 'h' :
+			default :
+				displayHelp();
+				return 0;
+		}
+
+		i++;
+	}
+
+
 	std::string a,b;
 	getline(std::cin,a);
 	getline(std::cin,b);
@@ -25,6 +48,11 @@ int main (void)
 #endif
 
 	std::cout << computer.Distance() << std::endl;
-	computer.PrintAlignment(std::cout);
+
+	if ( AlignOption )
+	{
+		computer.PrintAlignment(std::cout);
+	}
+
 	return 0;
 }
