00972b72a1383e38940da2a7345aca8e6f70f19d
Patrick Finnerty <finnertypatri@aol.fr>
2017-11-30 14:54:32 +0900

    Re-wrote tests scripts to make then runable Added a .gitignore file to prevent committing the generated test output summary

--- tests/test.sh
+++ tests/test.sh
@@ -1,31 +1,21 @@
 #! /bin/bash
-echo "-----------------------------------------------------------"
-
+echo "--------------------------------------------------------------"
 execDir=`pwd`
 
-if [ "$1" = "" ]
+if [ -d "$1" ]
 then
-  echo "No directory given, default to current"
-  Directory="."
+	Directory="$1"
 else
-  echo "| Test id : $1"
-  if [ -d "$1" ]
-  then
-     Directory="$1"
-  else
-     echo "$1 is not a directory. Exiting."
-     exit 2
-  fi
+	echo "Not a directory, exiting"
+	exit 2
 fi
 
 cd $Directory
 
 if [ -r "description" ]
 then
-  echo "-----------------------------------------------------------"
-  echo "Description :"
-  fold description -w 60 -s
-  echo "-----------------------------------------------------------"
+	fold description -w 80 -s
+	echo "--"
 fi
 
 nTestCount=0
@@ -34,144 +24,88 @@ nStrResult="$1 "
 
 if [ -r "run" ]
 then
-  sRun=`cat run`
+	sRun=`cat run`
 else
-  echo "No run file found. Exiting."
-  exit 2
+	echo "No run file, exiting"
+	exit 2
 fi
 
-# stdin has been specified
+#stdin specified
 if [ -r "std.in" ]
-then
-  sRun="$sRun <std.in"
+then 
+	sRun="$sRun <std.in"
 fi
 
-# stdout has been specified
-if [ -r "std.out" ]
+#stdout specified
+if [ -r "stdout" ]
 then
-  sRun="$sRun >temp.txt"
+	sRun="sRun > temp.txt"
 fi
 
-# stderr has been specified
+#stderr specified
 if [ -r "stderr.out" ]
-then
-  sRun="$sRun 2>temperr.txt"
+then 
+	sRun="$sRun 2>temperr.txt"
 fi
 
 echo $sRun
-# execute the command line
 eval $sRun
 returnCode=$?
 
 resultGlobal=1
 
-# compare return code if concerned
-resultRC=2
-if [ -r "returncode" ]
-then
-  if [ "$returnCode" = `cat returncode` ]
-  then
-    echo "                                       Return Code : PASSED"
-    resultRC=1
-  else
-    echo "                                       Return Code : FAILED"
-    resultRC=0
-    resultGlobal=0
-  fi
-fi
+#Could compare return code
 
-# compare stdout if concerned
 resultOut=2
 if [ -r "std.out" ]
 then
-  diff -wB temp.txt std.out >/dev/null
-  if [ $? -eq 0 ]
-  then
-    echo "                                       Stdout      : PASSED"
-    resultOut=1
-  else
-    echo "                                       Stdout      : FAILED"
-    resultOut=0
-    resultGlobal=0
-  fi
-  # clean temporary out file
-  rm temp.txt
+	diff -wB temp.txt std.out >/dev/null
+	if [ $? -eq 0 ]
+	then 
+		echo "STDOUT : Passed"
+		resultOut=1
+	else
+		echo "STDOUT : Failed"
+		resultOut=0
+		resultGlobal=0
+	fi
+	rm temp.txt
 fi
 
-# compare stderr if concerned
-resultErr=2
 if [ -r "stderr.out" ]
-then
-  diff -wB temperr.txt stderr.out >/dev/null
-  if [ $? -eq 0 ]
-  then
-    echo "                                       Stderr      : PASSED"
-    resultErr=1
-  else
-    echo "                                       Stderr      : FAILED"
-    resultErr=0
-    resultGlobal=0
-  fi
-  # clean temporary out file
-  rm temperr.txt
+then 
+	diff -wB temperr.txt stderr.out >/dev/null
+	if [ $? -eq 0 ]
+	then 
+		echo "STDERR : Passed"
+		resultOut=1
+	else
+		echo "STDERR : Failed"
+		resultErr=0
+		resultGlobal=0
+	fi
+	rm temperr.txt
 fi
 
-# compare files created if concerned
-resultFiles=2
-if ls *.outfile &> /dev/null
-then
-  number=1
-  for i in *.outfile
-  do
-    fileName=`basename $i .outfile`
-    if [ -r $fileName ]
-    then
-      diff -wB $i $fileName
-      if [ $? -eq 0 ]
-      then
-        echo "                                       File #$number     : PASSED"
-      else
-        echo "                                       File #$number     : FAILED"
-        resultFiles=0
-        resultGlobal=0
-      fi
-      rm $fileName
-    else
-      echo "                                       File #$number     : FAILED"
-      resultFiles=0
-      resultGlobal=0
-    fi
-    let "number=$number+1"
-  done
-  if [ $resultFiles -eq 2 ]
-  then
-    resultFiles=1
-  fi
-fi
 
-echo "                                       --------------------"
+echo "-------------------------------------------------------------------"
 if [ $resultGlobal -eq 0 ]
 then
-  echo "                                       Global      : FAILED"
+	echo "GLOBAL : Failed"
 else
-  echo "                                       Global      : PASSED"
+	echo "GLOBAL : Passed"
 fi
-echo "-----------------------------------------------------------"
-echo
 
+#Write to the csv file provided
 cd $execDir
-
-# log result in $2 if filename provided
-if [ "$2" != "" ]
+if [ ! -w "$2" ]
+then 
+	touch $2
+fi
+if [ -w "$2" ]
 then
-  if [ ! -w "$2" ]
-  then
-    touch $2
-  fi
-  if [ -w "$2" ]
-  then
-    echo "$Directory;$resultRC;$resultOut;$resultErr;$resultFiles;$resultGlobal" >>$2
-  fi
+	echo
+	"$Directory;$resultOut;$resultErr;$resultGlobal" >>$2
 fi
 
 exit $resultGlobal
